1cc8d5efaf55b52adee3a96f89f62d64697360f4
ProtossLINK <LINK-21@yandex.ru>
2019-11-20 15:01:38 +0300

    Initial commit

---
+++ src/task5.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class task5 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число от 1 до 10 = ");
+        int num = in.nextInt();
+        System.out.println(num * 1);
+        System.out.println(num * 2);
+        System.out.println(num * 3);
+        System.out.println(num * 4);
+        System.out.println(num * 5);
+        System.out.println(num * 6);
+        System.out.println(num * 7);
+        System.out.println(num * 8);
+        System.out.println(num * 9);
+        System.out.println(num * 10);
+    }
+}
