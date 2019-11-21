24ff3b76f0852ba4ec465849f68dfe29c94cd71e
ProtossLINK <ProtossLINK@gmail.com>
2019-11-21 22:59:04 +0300

    task8

---
+++ src/task8.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+
+public class task8 {
+
+    public static void main(String[] args) {
+        int num = 1;
+        while (num < 51) {
+            System.out.println(num);
+            num = ++num;
+        }
+    }
+}
