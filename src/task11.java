22f8c191e6a690e8784d9e3e3859990af42371ac
ProtossLINK <LINK-21@yandex.ru>
2019-11-27 09:40:00 +0300

    task11

---
+++ src/task11.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+
+public class task11 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите текст = ");
+        String text = in.nextLine();
+        System.out.println(text);
+
+    }
+}
+
+
