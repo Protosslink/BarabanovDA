b949fd7aa21e13f7f3c35d9fb2f71352a2e378dc
ProtossLINK <LINK-21@yandex.ru>
2019-11-29 15:48:39 +0300

    4

--- src/task4.java
+++ src/task4.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
         public static void main(String[] args) {
 
-            Scanner input = new Scanner( System.in );
+            Scanner input = new Scanner(System.in);
             System.out.print("Введите число в бинарном формате = ");
             int binarynum = input.nextInt();
 
@@ -17,20 +17,37 @@ import java.util.Scanner;
             String[] strArr = strBinaryNum.split("");
             System.out.println(Arrays.toString(strArr));
 
-            int binary=binarynum;
-            int decimal = 0;
-            int power = 0;
-            while(true){
-                if(binary == 0){
-                    break;
+            for (int i = 0; i <= strArr.length; i++) {
+                int i3 = 0;
+                try {
+                    i3 = Integer.parseInt(strArr[i]);
+                    System.out.print(i3 + " ");
+                } catch (NumberFormatException e) {
+                    System.err.println("Неверный формат строки!");
+                }
+                if (i3 >= 2) {
+                    System.out.println("Введено число отличное от бинарного формата!");
                 } else {
-                    int temp = binary%10;
-                    decimal += temp*Math.pow(2, power);
-                    binary = binary/10;
-                    power++;
+                    int binary = binarynum;
+                    int decimal = 0;
+                    int power = 0;
+                    while (true) {
+                        if (binary == 0) {
+                            break;
+                        } else {
+                            int temp = binary % 10;
+                            decimal += temp * Math.pow(2, power);
+                            binary = binary / 10;
+                            power++;
 
+                        }
+                    }
                 }
             }
         }
     }
+
+
+
+
 
