de980134fefb63d6b1318a2e65a5fee6f717cb57
ProtossLINK <ProtossLINK@gmail.com>
2019-12-16 21:42:10 +0300

    Итоговая задача №2

--- src/finalTask2.java
+++ src/finalTask2.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class finalTask2 {
@@ -8,15 +9,29 @@ public class finalTask2 {
         System.out.print("Введите размер массива: ");
         int inSizeNum = inputSizeArr.nextInt();
         // System.out.println(inSizeNum);
-        int inNum;
+
         int[] arrInt = new int[inSizeNum];
         for (int i = 0; i < inSizeNum; i++) {
             System.out.print("Введите число: ");
             arrInt[i] = inputSizeArr.nextInt();
             //System.out.print(arrInt[i] + " ");
         }
-}
-}
+        int inNum;
+        for (int i = 1; i < arrInt.length; i++) {
+            inNum = arrInt[i];
+            int j = i - 1;
+            while(j >= 0 && arrInt[j] > inNum) {
+                arrInt[j + 1] = arrInt[j];
+                j = j - 1;
+            }
+            arrInt[j + 1] = inNum;
+            }
+        System.out.println(Arrays.toString(arrInt));
+        }
+    }
+
+
+
 
 
 
